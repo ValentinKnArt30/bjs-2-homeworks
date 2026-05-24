fcbab565fdbbc8be73f534a2972e0f80e9b7fed7
Valentin Knyshov <v.a.knishov@bk.ru>
2026-05-24 17:47:03 +0300

    Third  HW complite

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	return arr1.every((elm, index) => {
+		return elm === arr2[index];
+	});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let arr = users;
+
+	let result = arr
+		.filter(user => user.gender === gender)
+		.map(user => user.age)
+		.reduce((sum, age, _, arr) => sum + age / arr.length, 0);
+
+	return result;
 }
